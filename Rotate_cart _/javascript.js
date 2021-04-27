9522e18fec66ebf89a49228799ffa2cc2e4c9975
nickolasyarosh <83294348+nickolasyarosh@users.noreply.github.com>
2021-04-27 19:31:05 +0300

    Add files via upload

---
+++ Rotate_cart _/javascript.js
@@ -0,0 +1,16 @@
+//rotate
+function myFunction(e) {
+ e.classList.add("squareclick");
+ console.log(e);
+}
+
+var vid = document.getElementById("myVideo"); 
+
+function playVid() { 
+  vid.play(); 
+} 
+
+
+
+
+
